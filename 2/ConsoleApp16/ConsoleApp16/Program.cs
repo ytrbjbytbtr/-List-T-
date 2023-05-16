1ed4ef6ed2947fdd36fbd38bea55b94841a20a32
ytrbjbytbtr <52233543+ytrbjbytbtr@users.noreply.github.com>
2023-05-16 10:12:53 +0300

    Add files via upload

---
+++ 2/ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xceed.Wpf.Toolkit;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "/Users/eugene/Documents/app/Text1.txt";
+
+            string[] ArString = text.Split(new char[] { ' ' });
+            Dictionary<string, int> dr = new Dictionary<string, int>();
+
+            foreach (string s in ArString)
+                if (dr.Keys.Contains(s)) dr[s]++;
+                else dr.Add(s, 1);
+            string S = ""; int k = 0;
+            foreach (KeyValuePair<string, int> kk in dr.OrderByDescending(x => x.Value))
+            {
+                S += kk.Key + " " + kk.Value.ToString() + "\n";
+                if (k = 10) break;
+            }
+            MessageBox.Show(S);
+        }
+    }
+}
