5102e2e69c4f0ecc8c54b08b355b5d5a63ff6ec8
ytrbjbytbtr <52233543+ytrbjbytbtr@users.noreply.github.com>
2023-05-16 10:16:09 +0300

    Add files via upload

---
+++ 1/ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myListOfItemsToWatch = new List<object> { "foo", "bar", "baz" };
+            var listOfStopwatches = myListOfItemsToWatch.ToDictionary(watchItem =>
+                    watchItem, i => new Stopwatch()
+            );
+        }
+    }
+}
